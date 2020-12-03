724b23a808c99fcf392476ce59906e7532c47f6b
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-03 12:30:00 +0000

    Add structopt argument parsing

--- rp_cli/src/main.rs
+++ rp_cli/src/main.rs
@@ -1,3 +1,21 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Simple profiler for processes
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rp")]
+struct Opt {
+    #[structopt(short, long)]
+    time: bool,
+
+    #[structopt(short, long)]
+    memory: bool,
+
+    #[structopt(short, long, parse(from_os_str))]
+    command: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
 }
