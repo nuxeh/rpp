50727f65a9368833adf8b2c622e7029f7acab3bf
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-03 13:17:10 +0000

    Add pieces of API

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use std::process::Command;
-use failure::Error;
+use std::time::{Duration, Instant};
+use failure::{Error, bail};
 
 #[derive(Default)]
 pub struct Rpp {
     time: bool,
     memory: bool,
+    command: Option<String>,
+    results: Results,
 }
 
 impl Rpp {
@@ -22,15 +25,43 @@ impl Rpp {
         self
     }
 
+    pub fn command(mut self, command: &str) -> Self {
+        self.command = Some(command.to_owned());
+        self
+    }
+
     pub fn init(mut self) -> Self {
         self
     }
 
-    pub fn run(&self) -> Result<(), Error> {
+    pub fn run(&mut self) -> Result<(), Error> {
+        if self.command.is_none() {
+            bail!("Command hasn't been set");
+        }
+
+        let start = Instant::now();
+
+        let nanos = start.elapsed().as_nanos();
+        self.results.nanos = Some(nanos);
         Ok(())
     }
 }
 
+#[derive(Default)]
+pub struct Results {
+    memory: Option<usize>,
+    nanos: Option<u128>,
+}
+
+impl Results {
+    fn get_memory(&self) -> Option<usize> {
+        self.memory
+    }
+
+    fn get_nanos(&self) -> Option<u128> {
+        self.nanos
+    }
+}
 
 #[cfg(test)]
 mod tests {
